9ee6c6961b2bb16dbf06cf1810992d05921b4272
Thibaud REY <contact@thibaudrey.pro>
2022-03-20 19:52:21 +0100

    Native framerate

--- src/lib/node-rtsp-recorder/src/helpers/recorder.js
+++ src/lib/node-rtsp-recorder/src/helpers/recorder.js
@@ -65,7 +65,7 @@ const RTSPRecorder = class {
   }
 
   getChildProcess(fileName) {
-    var args = ['-i', this.url]
+    var args = ['-re', '-i', this.url]
     const mediaArgs = this.getArguments()
     mediaArgs.forEach((item) => {
       args.push(item)
